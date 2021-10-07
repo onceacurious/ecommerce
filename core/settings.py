f4e9f0da8b1e04e9ea085c3614f4a5ce753b988e
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2021-10-07 10:14:46 +0800

    Update Secret Key

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,8 @@
 
 import os
 from pathlib import Path
+from decouple import config
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0xaevr%t*@e@w4gdd522++pt2nuu-$@9(&yo2ef&_jmx$9bkx8'
+f = open('.env\\secret.json')
+secret = json.load(f)
+SECRET_KEY = secret['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +41,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
